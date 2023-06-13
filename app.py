977b80094488bf843e1d39a52c6610d46797cb7a
jmcaldwell88 <115884261+jmcaldwell88@users.noreply.github.com>
2023-06-12 20:30:31 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+# Import the dependencies.
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+
+#################################################
+# Database Setup
+#################################################
+app = Flask(__Hawaii__)
+
+# Configure the database connection
+app.config['Hawaii.sqlite"'] = 'Hawaii'
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+if __Hawaii__ == '__main__':
+    app.run()
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
